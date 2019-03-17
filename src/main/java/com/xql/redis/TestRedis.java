1fb57cbd8ae1bd5ef8b50a8c270d246015e86a14
fengzhongguji <15883788762@163.com>
2019-03-17 16:21:42 +0800

    redis的基本数据类型操作之set操作

--- src/main/java/com/xql/redis/TestRedis.java
+++ src/main/java/com/xql/redis/TestRedis.java
@@ -82,4 +82,23 @@ public class TestRedis {
         jedis.rpush("java framework","hibernate");
         System.out.println(jedis.lrange("java framework",0,-1));
     }
+    /**
+     * jedis操作Set
+     */
+    @Test
+    public void testSet(){
+        //添加
+
+        jedis.sadd("user1","liulings");
+        jedis.sadd("user1","xinxin");
+        jedis.sadd("user1","ling");
+        jedis.sadd("user1","zhangxinxin");
+        jedis.sadd("user1","who");
+        //移除noname
+        jedis.srem("user1","who");
+        System.out.println(jedis.smembers("user1"));//获取所有加入的value
+        System.out.println(jedis.sismember("user1", "who"));//判断 who 是否是user集合的元素
+        System.out.println(jedis.srandmember("user1"));
+        System.out.println(jedis.scard("user1"));//返回集合的元素个数
+    }
 }
