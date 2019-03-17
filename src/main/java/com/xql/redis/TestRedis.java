418782f971cff7b63a9506c261cbdef81e4ed649
fengzhongguji <15883788762@163.com>
2019-03-17 15:57:14 +0800

    redis的基本数据类型操作之list操作

--- src/main/java/com/xql/redis/TestRedis.java
+++ src/main/java/com/xql/redis/TestRedis.java
@@ -59,7 +59,27 @@ public class TestRedis {
             String next = iterator.next();
             System.out.println(next+":"+jedis.hmget("user",next));
         }
-        ArrayList
     }
+    /**
+     * jedis操作List
+     */
+    @Test
+    public void testList(){
+        //开始前，先移除所有的内容
+        jedis.del("java framework");
+        System.out.println(jedis.lrange("java framework",0,-1));
+        //先向key java framework中存放三条数据
+        jedis.lpush("java framework","spring");
+        jedis.lpush("java framework","struts");
+        jedis.lpush("java framework","hibernate");
+        //再取出所有数据jedis.lrange是按范围取出，
+        // 第一个是key，第二个是起始位置，第三个是结束位置，jedis.llen获取长度 -1表示取得所有
+        System.out.println(jedis.lrange("java framework",0,-1));
 
+        jedis.del("java framework");
+        jedis.rpush("java framework","spring");
+        jedis.rpush("java framework","struts");
+        jedis.rpush("java framework","hibernate");
+        System.out.println(jedis.lrange("java framework",0,-1));
+    }
 }
